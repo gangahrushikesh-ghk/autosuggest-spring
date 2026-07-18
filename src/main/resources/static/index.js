feedd3726584718218c95b30c62973aaabaecf86
gangahrushikesh <ganga.hrushikesh2008@gmail.com>
2026-07-18 12:17:05 +0530

    changes in html css and toggle js

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
     audio.loop = true;
     
     var source = document.createElement('source');
-    source.src = 'your-audio-file.mp3';
-    source.type = 'audio/mpeg';
+    source.src = 'khaleja_bgm.mp3';
+    source.type = 'audio/mp3';
     
     audio.appendChild(source);
     document.body.appendChild(audio);
@@ -14,3 +14,40 @@ document.addEventListener('DOMContentLoaded', function() {
         audio.play();
     }, { once: true });
 });
+
+var users = [
+    {
+        "name" : "prabhas",
+        "gender" : "Male",
+        "img" : "john.png"
+    },
+    {
+        "name" : "Anushka",
+        "gender" : "Female",
+        "img" : "jane.png"
+    }
+]
+
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1)%2
+    // get the elements
+
+    // 1: image
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+
+    // 2: name
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+
+    // 3: gender
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = users[id].gender;
+
+}
+function randomUser(){
+    id = Math.floor(Math.random() * 2); 
+    toggleUser();
+}
